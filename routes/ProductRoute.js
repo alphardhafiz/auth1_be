8c15b2b48268b61a38aad356f34feed0c4bcfe1b
alphardhafiz <bucialphard@gmail.com>
2023-11-05 12:51:27 +0700

    setting router

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/products", getProducts);
 router.get("/products/:id", getProductById);
 router.post("/products", createProduct);
-router.patcj("/products/:id", updateProduct);
+router.patch("/products/:id", updateProduct);
 router.delete("/products/:id", deleteProduct);
 
 export default router;
